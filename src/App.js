81ffe8cdd33990a8ea1a0059ab5badf9dc3623ee
MariSeest <seest97@gmail.com>
2024-06-15 20:33:42 +0000

    connectionBE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./Home";
@@ -12,6 +12,24 @@ import {useAuth0} from "@auth0/auth0-react";
 import {AuthenticationGuard} from "./components/authentication-guard";
 import ClosedTickets from "./ClosedTickets"
 
+function ConnectionBE(){
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+
+        fetch("http://localhost:3001/test")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+
+
+    }, []);
+
+        return (
+            <div className="ConnectionBE">
+            <h1>{message}</h1>
+            </div>
+        );
+}
 
 function PageNotFound() {
     return <h1>Page not Found</h1>;
@@ -33,6 +51,7 @@ function App() {
             <Route path="/visualizzaticket" element={<AuthenticationGuard component={VisualizzaTicket} />}/>
             <Route path="/apriunticket" element={<AuthenticationGuard component={ApriUnTicket} />}/>
             <Route path="/closedtickets" element={<AuthenticationGuard component={ClosedTickets} />} />
+            <Route path="/ConnectionBE" element={<AuthenticationGuard component={ConnectionBE} />} />
             <Route
                 path="*"
                 element={<PageNotFound />}
