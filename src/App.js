dbbf30e9995cba9838360eb2a3eccb2ef1cfda28
MariSeest <seest97@gmail.com>
2024-10-01 14:42:32 +0200

    modifiche tickets

--- src/App.js
+++ src/App.js
@@ -21,16 +21,6 @@ import ErrorBoundary from './ErrorBoundary';  // Importa ErrorBoundary
 
 function App() {
     const { isLoading } = useAuth0();
-    const [data, setData] = useState(null);
-    const apiUrl = process.env.REACT_APP_API_URL;
-
-    useEffect(() => {
-        fetch(`${apiUrl}/data`)
-            .then(response => response.json())
-            .then(data => setData(data))
-            .catch(error => console.error('Error fetching data:', error));
-    }, [apiUrl]);
-
     if (isLoading) {
         return <div><PageLoader /></div>;
     }
